3dead26c85dbd23c8fbe9916edd3b3a0bd296186
egrunin <rregtc@hotmail.com>
2016-02-18 11:35:51 -0500

    fourth commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,70 +3,33 @@
 // setup your IIFE (Immediately Invoked Function Expression)
 (function () {
 
+var checkMeOut;
 
-    var paragraphData = [{"paragraph":"First Paragraph Stuff"}, 
-                        {"paragraph":"Second Paragraph Stuff"},
-                        {"paragraph":"Third Paragraph Stuff"},
-                        {"paragraph":"Fourth Paragraph Stuff"}];
+// searching the entire page for 'checkMeOut'
+checkMeOut = document.getElementById("checkMeOut");
 
-    console.log("App Started..."); 
-  
-  /* the ugly way
-    console.log(paragraphData[1]);
-    console.log(paragraphData[1]);
-    console.log(paragraphData[2]);
-    console.log(paragraphData[0]);
-    console.log(paragraphData[5]);
-    console.log(paragraphData[6]);
-  */
-  
-    // a little nicer but counting up
-    
-    var paragraphDataLength = paragraphData.length;
-    for(var index= 0; index < paragraphDataLength; index++) {
-        console.log(paragraphData[index].paragraph);
-    }
-    
-    var name = document.getElementById("name");
-    
-    var button = document.getElementById("Button1");
-    
-    button.addEventListener("click", function(){
-        // execute stuff here after button click
-        console.log("Clicked!!");
-        console.log(name.value);
-    });
-    
-   
-     // a little nicer but counting down
-    /*
-    for(var index= paragraphData.length-1; index > -1; index--) {
-        console.log(paragraphData[index]);
-    }
-    */
-    
-    
-    /* even nicer but more complicated
-    paragraphData.forEach(function(paragraph) {
-        console.log(paragraph);
-    });
-    */
-  
-    //declared a named function way
 
-    function replaceFirstParagraph() {
-        console.log("inside replaceFirstParagraph function");
-        var firstParagraph;
+/*
+cheMeOut.onclick(function(){
+	console.log("checkMeOut clicked");
+});
+*/
 
-        firstParagraph = document.getElementById("firstParagraph");
 
-        firstParagraph.innerHTML = "My New Paragraph Data";
-        
-    }
+// bind an event listener to the 'checkMeOut' element
+checkMeOut.addEventListener("click", function(){
+	
+	if(checkMeOut.checked){
+		console.log("It's checked");
+	} else {
+		console.log("It's unchecked");
+	}
+	
+	/* ternary form
+	(checkMeOut.checked)? console.log("It's checked") : console.log("It's unchecked");
+	*/
+});
 
-   
-    // call the replaceFirstParagraph function
-    replaceFirstParagraph();
 
 
 })();
